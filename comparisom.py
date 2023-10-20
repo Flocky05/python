8890a324622e1c20de344fdb054c81ca4c052b0d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-20 20:49:19 +0600

    update comparisom section

---
+++ comparisom.py
@@ -0,0 +1,7 @@
+""" In python here some comparisiom section like >,<,>=,!= """
+a = int(input("enter the a: "))
+b = int(input("enter the b: "))
+if (a > b):
+    print("My name is Rakibul")
+else:
+    print("Hi,brother")
