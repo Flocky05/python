cdb2381baa1da5ca1bbe32453a883d4fca71bea1
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-20 21:33:37 +0600

    added while loop

---
+++ whileloop.py
@@ -0,0 +1,20 @@
+""" while is as like as other program """
+a = int(input('enter the number a: '))
+while (10 > a):
+    print("My Name is Rakibul Islam")
+    a += 1
+""" some time we have to need break and contineu, if you use break ,then condition fulfill then program will stoped and if you use contineu , when condition fulfill only skip the condition and contineu """
+
+b = int(input('enter the number b: '))
+while (10 > b):
+    print("My Name is Rakibul Islam")
+    b += 1
+    if (b == 5):
+        break
+
+c = int(input('enter the number c: '))
+while (10 > c):
+    print("My Name is Rakibul Islam")
+    c += 1
+    if (c == 5):
+        continue
