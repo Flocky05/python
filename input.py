144fc0abd554b174b0a3e45cfb013ee49b7856a2
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-20 15:44:53 +0600

    added input section

---
+++ input.py
@@ -0,0 +1,13 @@
+""" Here input is refered to user input, If anyOne try to input from user he can use input() function, and all time you have to mind it , python auto type generate and that type is string, so if you input other type you have to change the type and then use it. Otherwise you can't get right output """
+
+number1 = (input("number1: "))
+number2 = (input("number2: "))
+sum = number1+number2
+print("Total number is: ", sum)
+
+
+""" If you don't change type you don't get right output, And if you want to get right output you have change type, May you change type below way : """
+num1 = (input("number1: "))
+num2 = (input("number2: "))
+sumTotal = num1+num2
+print("Total number is: ", sumTotal)
