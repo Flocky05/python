3a0d8d06213efbd8e006f3d7098d1ec1716c9382
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-20 15:58:32 +0600

    added oparetor

---
+++ oparetor.py
@@ -0,0 +1,14 @@
+""" Python operator is the most important part as ussel for other language here is no change from other language Some operators(+,-,*,/,%) """
+number1 = 12
+number2 = 10
+sum = number1+number2
+div = number1/number2
+mul = number1*number2
+sub = number1-number2
+
+print(sum, div, mul, sub)
+
+""" If any one try to use power, he can use thats using two time ** """
+num = 2
+power = number2**num
+print(power)
