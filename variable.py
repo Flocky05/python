59d0ff1091bad9782ef7e1b75253b0c79d56670a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-20 15:25:04 +0600

    added variable section

---
+++ variable.py
@@ -0,0 +1,11 @@
+""" Using variable in python is so easy. you don't have to use variable type. You should use a variable name and use = sign and use a value not further more """
+a = 10
+b = 20
+sum = a+b
+print(sum)
+name = 'rakibul'
+print(name)
+
+""" If anyone try to use variable input , who can use to  f like: """
+me = f'i am a student , and my age is{sum}, my name is md. {name} Islam'
+print(me)
