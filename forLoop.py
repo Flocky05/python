24f9e14f51f7c859a43ccc17e5d64cc534906496
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-21 14:19:24 +0600

    added for loop section

---
+++ forLoop.py
@@ -0,0 +1,8 @@
+""" As we can't use comma(,),semicolone(;) so for loop will others way, the syntax are for loop like:
+for arrayelementName in arrayName:
+
+ """
+students = ['rakib', 'sakib', 'adnan', 'tipu', 'mehedi', 'emon', 'shanto']
+
+for student in students:
+    print(student)
