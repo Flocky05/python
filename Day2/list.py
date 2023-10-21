6de2e55267d531a42affe0e06c86d74fb7ab2489
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-21 23:20:34 +0600

    added list

---
+++ Day2/list.py
@@ -0,0 +1,7 @@
+""" list one kind of array  """
+numbers = [54, 223, 65, 98, 45, 6, 23, 65, 63, 55, 36, 3, 45, 54]
+print(numbers)
+print(numbers[7])
+print(numbers[-3])
+print(numbers[1:7])
+print(numbers[1:8:3])
