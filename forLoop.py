3772649e21949f74a0fe463c31c13b913d168129
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-21 14:21:09 +0600

    use range

--- forLoop.py
+++ forLoop.py
@@ -6,3 +6,8 @@ students = ['rakib', 'sakib', 'adnan', 'tipu', 'mehedi', 'emon', 'shanto']
 
 for student in students:
     print(student)
+
+""" Range if you want to print out natural number may u use range(statr,end) """
+
+for number in range(1, 10):
+    print(number)
