3546ddbe03cc1a82db72b1a030b7a05095c890ab
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-21 22:56:37 +0600

    buildin function

---
+++ Day2/build_in_function.py
@@ -0,0 +1,6 @@
+
+array = [2, 6, 9, 8, 45, 2, 5, 3, 45, 96, 25]
+largest_number = max(array)
+smalest_number = min(array)
+print('The largest Number is: ', largest_number,
+      'The smalest Number is: ', smalest_number)
