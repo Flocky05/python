4b4840c1d888128164ac6f2cb35574fabae41086
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-23 21:48:02 +0600

    stirng some methods

--- Day3/string_explore.py
+++ Day3/string_explore.py
@@ -3,3 +3,12 @@ print(name)
 
 for char in name:
     print(char)
+
+
+sentence = """who are you bro? what's the wrong with you bro?
+    even I don't know you
+ """
+print(sentence)
+print(sentence.upper())
+print(sentence.title())
+print(sentence.capitalize())
