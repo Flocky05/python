d95adf7d46fa74ff5b2dfc07faae06cb08e235b7
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-24 13:27:44 +0600

    added Dictionary

---
+++ Day3/Dictionary .py
@@ -0,0 +1,6 @@
+""" Known as key value pair, here must be a key and must be a value and start with {'key':value} """
+person = {'name': 'Rakibul', 'address': 'Tangail',
+          'Education': 'Dhaka University', 'Job': 'Work less'}
+print(person)
+print(person.values())
+print(person.keys())
