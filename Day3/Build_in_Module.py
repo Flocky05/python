ef95e90d7fa7eea7df821ef1b62f68970a9d8f19
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-24 19:00:09 +0600

    added shuffle module

--- Day3/Build_in_Module.py
+++ Day3/Build_in_Module.py
@@ -3,6 +3,10 @@ from random import *
 result = ceil(4.7)
 number = round(343.34)
 print(result, number)
-
+x = [12, 1, 2, 3, 4, 5, 6, 32, 21, 23, 34, 45, 45, 56, 54, 43, 32]
 num = random()
 print('This is random number: ', round(num*10))
+print('The winner is : ', choice(['sakib', 'rakib', 'niloy', 'sobuj',
+      'mehedi', 'emon', 'milon', 'rohan']))
+shuffle(x)
+print(x)
