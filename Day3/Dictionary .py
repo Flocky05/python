d992d4698ced2dc2453fc32e5b79ba55738a3665
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-24 13:45:38 +0600

    added new element in dict

--- Day3/Dictionary .py
+++ Day3/Dictionary .py
@@ -1,6 +1,14 @@
-""" Known as key value pair, here must be a key and must be a value and start with {'key':value} """
+""" a dictionary can be created by placing a sequence of elements within curly {} braces, separated by ‘comma’. Dictionary holds pairs of values, one being the Key and the other corresponding pair element being its Key:value. Values in a dictionary can be of any data type and can be duplicated, whereas keys can’t be repeated and must be immutable. """
 person = {'name': 'Rakibul', 'address': 'Tangail',
           'Education': 'Dhaka University', 'Job': 'Work less'}
 print(person)
+print(person['Job'])
+person['Language'] = 'python'
+print(person)
 print(person.values())
 print(person.keys())
+
+""" we can also create nested dictionary """
+friend = {'id': {'name': 'mehedi', 'address': 'BagerHat', 'versity': 'FUET'},
+          'father_Name': 'Momen Khan', 'Mother_Name': 'Johura Begum'}
+print(friend)
