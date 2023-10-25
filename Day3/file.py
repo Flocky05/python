67c53cdf035be9abaccb45590f476fbf921a6b53
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-26 00:21:43 +0600

    append a file

--- Day3/file.py
+++ Day3/file.py
@@ -1,7 +1,12 @@
+""" For create a file and write something in the file """
 with open('message.text', 'w') as file:
     for i in range(7):
         file.write("Who are you bro?\n")
 
+with open('message.text', 'a') as file:
+    file.write('My name is Rakibul Islam')
+
+"""  for read the file """
 with open('message.text', 'r') as file:
     content = file.read()
 
