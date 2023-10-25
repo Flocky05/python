7051fe2e348e33a26f9dfa258fa369b9b2af1da7
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-26 01:22:12 +0600

    added lambda function

---
+++ Day3/lemda.py
@@ -0,0 +1,11 @@
+# lamda is one kind of small functin declaration procedure ,That means the syntax is like
+""" functionName = lambda variale: value """
+
+
+""" def doubled(num):
+    return num*2 """
+# thus function can be created by others way like:
+def doubled(num): return num*2
+
+
+print(doubled(2))
