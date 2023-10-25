5a717f95d936a98486367d0cfb37cf3141c798a5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-26 01:37:33 +0600

    update lambda function

--- Day3/lemda.py
+++ Day3/lemda.py
@@ -9,3 +9,15 @@ def doubled(num): return num*2
 
 
 print(doubled(2))
+
+
+actors = [
+    {'name': 'sabana', 'age': 56},
+    {'name': 'sibla', 'age': 20},
+    {'name': 'Sakib Khan', 'age': 51},
+    {'name': 'Purnima', 'age': 45},
+    {'name': 'Suhasini', 'age': 21},
+]
+
+juniors = filter(lambda actor: actor['age'] < 21, actors)
+print(list(juniors))
