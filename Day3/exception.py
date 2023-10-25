e12c7a0915b0339c77a50652e5bcf3f6ef0a1ce5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-25 23:59:22 +0600

    update

---
+++ Day3/exception.py
@@ -0,0 +1,9 @@
+try:
+    number = 12/0
+except:
+    print("Here is an error")
+
+finally:
+    print("Code is here brother")
+
+print("This is so hard code , if you guss something here happend any problem then you must be use try , except issue, then finally is used for  we ")
