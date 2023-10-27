972b89cdd16cd7be53cf0e66d23f49f6e978f5f2
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-27 23:59:17 +0600

    update enums

---
+++ Day4/enums.ts
@@ -0,0 +1,10 @@
+/* Some time u have to need more types in a common environment that time you can you use it */
+enum setChoice {
+  AISLE = 27,
+  MIDDLE,
+  WINDOW,
+  FOURTH,
+}
+
+const seat = setChoice.MIDDLE;
+console.log(seat);
