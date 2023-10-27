2d5fa9e7b4a6c16a0d84bd48c439d0d35104f9e3
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-28 00:01:10 +0600

    added description enums

--- Day4/enums.ts
+++ Day4/enums.ts
@@ -1,4 +1,5 @@
 /* Some time u have to need more types in a common environment that time you can you use it */
+/* Enums are a feature added to JavaScript by TypeScript which allows for describing a value which could be one of a set of possible named constants. Unlike most TypeScript features, this is not a type-level addition to JavaScript but something added to the language and runtime. */
 enum setChoice {
   AISLE = 27,
   MIDDLE,
