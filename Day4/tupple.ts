af1d251258ee05cb4ea80f7f0c68f77b24eae13b
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-27 23:31:26 +0600

    update tupple section

---
+++ Day4/tupple.ts
@@ -0,0 +1,5 @@
+/* Tupple means that each and every types will be same position in the array otherwise its show wrong */
+let employ: [string, number] = ["kulsum", 45];
+
+let user: [string, number, boolean] = ["mskulsum@gmail.com", 123, true];
+console.log(user);
