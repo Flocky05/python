9794eb2f603a370e6ea0e25800ddce58f89fc481
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-27 19:06:06 +0600

    added array

---
+++ Day4/array.ts
@@ -0,0 +1,4 @@
+let fruites: string[] = ["Mango", "Apple", "Banana", "Orange"];
+fruites.push("Tomato");
+//Here must every element will be string type other ways its gonna be show errors
+console.log(fruites);
