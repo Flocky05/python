6dced8b0032d14c74ff322ed5126bd93fb3010a3
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-27 19:12:10 +0600

    added the number

--- Day4/array.ts
+++ Day4/array.ts
@@ -2,3 +2,9 @@ let fruites: string[] = ["Mango", "Apple", "Banana", "Orange"];
 fruites.push("Tomato");
 //Here must every element will be string type other ways its gonna be show errors
 console.log(fruites);
+
+/* If the array will be numbered type element , you can not added other type elements so you have to cearfull about the type of elements */
+
+let number: number[] = [1, 56, 45, 89, 33, 45, 63, 61];
+number.push(20);
+console.log(number.sort());
